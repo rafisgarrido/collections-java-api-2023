ad5ecb74eb8c3cf7d21ce8bab8512af52881e0f5
rafisgarrido <rafisgarrido@gmail.com>
2023-10-11 08:59:33 -0300

    OrdenacaoNumeros resultado final

---
+++ src/Ordenacao/OrdenacaoNumeros.java
@@ -0,0 +1,61 @@
+package Ordenacao;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class OrdenacaoNumeros {
+    //atributos
+    private List<Integer> numerosList;
+
+    public OrdenacaoNumeros() {
+        this.numerosList = new ArrayList<>();
+    }
+
+    public void adicionarNumero(int numero){
+        this.numerosList.add(numero);
+    }
+
+    public List<Integer> ordenarAscendente() {
+        List<Integer> numerosAscendentes = new ArrayList<>(this.numerosList);
+        if (!numerosList.isEmpty()) {
+            Collections.sort(numerosAscendentes);
+            return numerosAscendentes;
+        } else {
+            throw new RuntimeException("A lista está vazia!");
+             }
+        }
+
+        public List<Integer> ordenarDescendente(){
+        List<Integer> numerosAscendentes = new ArrayList<>(this.numerosList);
+        if(!numerosList.isEmpty()){
+            numerosAscendentes.sort(Collections.reverseOrder());
+            return numerosAscendentes;
+        } else {
+            throw new RuntimeException("A lista está vazia!");
+        }
+    } public static void main(String[] args) {
+        // Criando uma instância da classe OrdenacaoNumeros
+        OrdenacaoNumeros numeros = new OrdenacaoNumeros();
+
+        // Adicionando números à lista
+        numeros.adicionarNumero(2);
+        numeros.adicionarNumero(5);
+        numeros.adicionarNumero(4);
+        numeros.adicionarNumero(1);
+        numeros.adicionarNumero(99);
+
+
+        // Ordenando e exibindo em ordem ascendente
+        System.out.println(numeros.ordenarAscendente());
+
+
+
+        // Ordenando e exibindo em ordem descendente
+        System.out.println(numeros.ordenarDescendente());
+
+    }
+
+}
+
